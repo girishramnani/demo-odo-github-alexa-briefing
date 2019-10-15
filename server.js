893dfd23d971595ad72ffe990ae421e1e1336659
Girish Ramnani <girishramnani95@gmail.com>
2019-10-16 02:20:48 +0530

    more things added

--- server.js
+++ server.js
@@ -14,19 +14,27 @@ const repo_identifier = "openshift/odo"
 const repo_name = "odo"
 
 function get_repo_and_issues() {
-  return axios.get(`http://${process.env.COMPONENT_PYTHON_REPO_ISSUE_A_NBXM_HOST}:${process.env.COMPONENT_PYTHON_REPO_ISSUE_A_NBXM_PORT}/${repo_identifier}`)
+  return axios.get(`http://${process.env.COMPONENT_PYTHON_REPO_ISSUE_A_HAZY_HOST}:${process.env.COMPONENT_PYTHON_REPO_ISSUE_A_HAZY_PORT}/${repo_identifier}`)
 }
 
 server.get("/", (req, res) => {
 
     get_repo_and_issues().then((response) => {
-      res.json({
+      res.json([{
         "uid": `urn:uuid:odo-${uuidv4()}`,
         "updateDate": new Date().toISOString(), // we set this as latest for now
         "titleText": `Github Issues for ${repo_name} on ${new Date().toDateString()}`,
-        "mainText": `There were ${response.data.last7days} issues opened in ${repo_name} since last 7 days`,
+        "mainText": `There were ${response.data.issues.last_7days} issues opened in ${repo_name} since last 7 days`,
         "redirectionUrl": `https://github.com/${repo_identifier}/issues`
-      })
+      },
+      {
+        "uid": `urn:uuid:odo-${uuidv4()}`,
+        "updateDate": new Date().toISOString(), // we set this as latest for now
+        "titleText": `Github Notifications for ${repo_name} on ${new Date().toDateString()}`,
+        "mainText": `There were ${response.data.notifications.yesterday} notifications on ${repo_name} repo since yesterday`,
+        "redirectionUrl": `https://github.com/notifications`
+      }
+    ])
     })
   
 })
