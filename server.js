0efd273aeb45a5c38f4267973b18c560e5eb1eb2
Girish Ramnani <girishramnani95@gmail.com>
2019-10-15 15:38:35 +0530

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 8080;
+const helmet = require('helmet');
+const compression = require('compression');
+const uuidv4 = require('uuid/v4');
+const server = express();
+
+server.use(compression());
+server.use(helmet());
+
+server.get("/", (req, res) => {
+    res.json({
+        "uid": `urn:uuid:odo-${uuidv4()}`,
+        "updateDate": new Date().toISOString(), // we set this as latest for now
+        "titleText": `Github Issues on ${new Date().toDateString()}`,
+        "mainText": "Currently you have no issues",
+        "redirectionUrl": "https://github.com/openshift/odo/issues"
+      })
+})
+
+server.listen(port, () =>
+console.log(`Listening on port ${port}`)
+);
